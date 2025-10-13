3163e1d2b9ff6a991c999e743e506311427e0b24
Olha Tykhonchuk <tykhonchuk.ola@gmail.com>
2025-10-13 20:15:46 +0200

    Added LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using lab1_gr1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using ListaZakupow.Model.DataModels;
+using lab1_gr1.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaZakupow.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+        private readonly PasswordHasher<User> _passwordHasher;
+        public LoginController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+            _passwordHasher = new PasswordHasher<User>();
+        }
+        
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == dto.Username);
+            if (user == null)
+            {
+                return Unauthorized("Niepoprawny login lub hasło");
+            }
+            var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, dto.Password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return Unauthorized("Niepoprawny login lub hasło");
+            }
+            return Ok("Zalogowano pomyślnie");
+        }
+
+        [HttpGet("check-user")]
+        public async Task<IActionResult> CheckUser([FromQuery]string username)
+        {
+            bool exists = await _dbContext.Users.AnyAsync(u => u.Username == username);
+            return Ok(new { exists });
+        } 
+        
+    }
+}
