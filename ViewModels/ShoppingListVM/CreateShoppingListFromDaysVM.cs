587998cbec6a95b6b18d01cef9bcf6563b802df2
Olha Tykhonchuk <tykhonchuk.ola@gmail.com>
2025-11-02 17:47:10 +0100

    added shoppinglist/+item and user view models

---
+++ ViewModels/ShoppingListVM/CreateShoppingListFromDaysVM.cs
@@ -0,0 +1,7 @@
+﻿namespace lab1_gr1.ViewModels.ShoppingListVM
+{
+    public class CreateShoppingListFromDaysVM
+    {
+        public List<int> SelectedDays { get; set; } = new();
+    }
+}
---
+++ ViewModels/ShoppingListVM/CreateShoppingListVM.cs
@@ -0,0 +1,13 @@
+﻿using lab1_gr1.ViewModels.ShoppingListItemVM;
+
+namespace lab1_gr1.ViewModels.ShoppingListVM
+{
+    public class CreateShoppingListVM
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<CreateShoppingListItemVM> Items { get; set; } = new();
+
+    }
+}
