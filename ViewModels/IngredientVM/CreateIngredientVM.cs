f9d6ed3d740ed4fbc6034506ef7c073acaad66ec
EwaKosmala <ewa.kosmala01@gmail.com>
2025-11-04 19:25:53 +0100

    Przeglądanie przepisu

---
+++ ViewModels/IngredientVM/CreateIngredientVM.cs
@@ -0,0 +1,7 @@
+﻿namespace lab1_gr1.ViewModels.IngredientVM
+{
+    public class CreateIngredientVM
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ViewModels/IngredientVM/EditIngredientVM.cs
@@ -0,0 +1,8 @@
+﻿namespace lab1_gr1.ViewModels.IngredientVM
+{
+    public class EditIngredientVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
