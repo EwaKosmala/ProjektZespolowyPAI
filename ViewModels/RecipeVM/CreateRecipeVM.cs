a88406ee4b062b2ef872b85bb11f5ebccdb0fac7
Olha Tykhonchuk <tykhonchuk.ola@gmail.com>
2025-11-19 21:46:46 +0100

    make sure that at least 1 ingredient was added to recipe

--- ViewModels/RecipeVM/CreateRecipeVM.cs
+++ ViewModels/RecipeVM/CreateRecipeVM.cs
@@ -6,14 +6,16 @@ namespace lab1_gr1.ViewModels.RecipeVM
     public class CreateRecipeVM
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Musisz podaæ nazwê przepisu")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Musisz podaæ opis przepisu")]
         public string Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Musisz podaæ instrukcje przygotowania")]
         public string Instructions { get; set; }
 
+        [MinLength(1, ErrorMessage = "Musisz dodaæ przynajmniej jeden sk³adnik")]
         public List<CreateRecipeIngredientVM> Ingredients { get; set; } = new List<CreateRecipeIngredientVM>();
         public List<CreateRecipeScheduleVM> Schedules { get; set; } = new();
         public List<int> SelectedDays { get; set; } = new();
