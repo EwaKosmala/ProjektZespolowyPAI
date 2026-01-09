4b03277536f00fa8224d9ab3ea954c8193a4b0b8
Olha Tykhonchuk <tykhonchuk.ola@gmail.com>
2026-01-09 22:28:18 +0100

    base controller

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -2,13 +2,30 @@
 
 namespace lab1_gr1.Controllers
 {
+    /// <summary>
+    /// Klasa bazowa dla kontrolerów aplikacji.
+    /// Zawiera wspólne metody pomocnicze związane z sesją użytkownika.
+    /// </summary>
     public class BaseController : Controller
     {
+        /// <summary>
+        /// Zwraca identyfikator aktualnie zalogowanego użytkownika
+        /// zapisany w sesji HTTP.
+        /// </summary>
+        /// <returns>
+        /// Identyfikator użytkownika zalogowanego w aplikacji.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Rzucany, gdy użytkownik nie jest zalogowany
+        /// lub brak identyfikatora w sesji.
+        /// </exception>
         public int GetUserId()
         {
             var userId = HttpContext.Session.GetInt32("UserId");
+
             if (userId == null)
                 throw new Exception("Użytkownik niezalogowany");
+
             return userId.Value;
         }
     }
